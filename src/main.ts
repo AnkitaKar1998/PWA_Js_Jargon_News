59452c4d17578e7d3d3a3eac81b8a8c12510576f
Ankita Kar <ankitakar98@gmail.com>
2021-01-10 16:44:00 +0530

    added workbox settings

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,19 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import {Workbox} from 'workbox-window'
+
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
+  .then(_=> loadServiceWorker())
   .catch(err => console.error(err));
+
+  function loadServiceWorker() {
+    if ('serviceWorker' in navigator) {
+     const wb = new Workbox('/sw.js');
+     wb.register();
+    }
+   }
